61b6918e38fe0bf4d941b40ccb9ae11cf2420aec
Nicholas Halloran <nmhalloran@gmail.com>
2018-05-03 09:03:08 -0700

    homework commit.

--- W3D3/db/migrate/20180502050237_create_peeps_table.rb
+++
@@ -1,9 +0,0 @@
-class CreatePeepsTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :peeps_tables do |t|
-      t.string :name, null: false
-      t.integer :house_id
-      t.timestamps
-    end
-  end
-end
--- W3D3/db/migrate/20180502050246_create_houses_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateHousesTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :houses_tables do |t|
-      t.string :address, null: false 
-      t.timestamps
-    end
-  end
-end
